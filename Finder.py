55e3022bb2b470542487db0196c0a29e5f07171f
Ahobba <infinitetimesorry+github@gmail.com>
2022-11-15 13:15:43 -0300

    Created Finder

---
+++ Finder.py
@@ -0,0 +1,90 @@
+from pyautogui import moveTo, click, press, hotkey
+from keyboard import write
+from time import sleep
+
+def action(site, nome, ferramenta, pesquisa):
+    moveTo(77, 752)
+
+    click(button='right')
+    sleep(0.5)
+
+    press('down')
+    sleep(0.1)
+    press('down')
+    sleep(0.1)
+    press('down')
+    press('enter')
+
+    sleep(7)
+    hotkey('win', 'up')
+    sleep(0.5)
+
+    if ferramenta == 2:
+        if pesquisa == 3:
+            write(f'{site}/search?q={nome}', 0.05) 
+
+        else:
+            write(f'{site}/?s={nome}', 0.05)
+    else:
+        write(f'{site}/?s={nome}', 0.05)
+            
+    sleep(0.2)
+
+    press('enter')
+
+
+opcoes = input('[1] animes\n[2] Jogos\n> ')
+thing = int(opcoes)
+
+if opcoes == '1':
+    
+    anime = input('\nNome do Anime:\n> ').replace(' ', '+').lower()
+
+    if anime == '':
+        print('\nNão tem como pesquisar "nada".\nPor favor, feche e abra o programa novamente!')
+        i = input()
+
+    else:
+        site = input('\n[1] Animes Zone\n> ')
+        number = int(site)
+
+        if site == '1':
+            site = 'animeszone.net'
+
+        else:
+            print('\nOpção inválida.\nPor favor, feche e abra o programa novamente!')
+            i = input()
+
+        
+        action(site, anime, thing, number)
+
+elif opcoes == '2':
+
+    game = str(input('\nNome do jogo:\n> ')).replace(' ', '+').lower()
+
+    if game == '':
+        print('\nNão tem como pesquisar "nada".\nPor favor, feche e abra o programa novamente!')
+        i = input()
+
+    else:
+        site = input('\n[1] Steam Unlocked\n[2] Br Jogos Torrent\n[3] The Pirate Jogos\n> ')
+        number = int(site)
+
+        if site == '1':
+            site = 'steamunlocked.net'
+            
+        elif site == '2':
+            site = 'brjogostorrents.com'
+
+        elif site == '3':
+            site = 'thepiratejogos.net'
+            
+        else:
+            print('\nOpção inválida.\nPor favor, feche e abra o programa novamente!')
+            i = input()
+
+        action(site, game, thing,number)
+
+else:
+    print('\nOpção inválida.\nPor favor, feche e abra o programa novamente!')
+    i = input()
