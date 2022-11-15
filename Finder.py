703ead3bba2cbfe32ce8cd02fe13e3aded710854
Ahobba <infinitetimesorry+github@gmail.com>
2022-11-15 14:34:35 -0300

    Update Finder.py

--- Finder.py
+++ Finder.py
@@ -20,8 +20,8 @@ def action(site, nome, ferramenta, pesquisa):
     hotkey('win', 'up')
     sleep(0.5)
 
-    if ferramenta == 2:
-        if pesquisa == 3:
+    if ferramenta == 2 or ferramenta == 3:
+        if pesquisa == 3 or pesquisa == 1:
             write(f'{site}/search?q={nome}', 0.05) 
 
         else:
@@ -34,7 +34,7 @@ def action(site, nome, ferramenta, pesquisa):
     press('enter')
 
 
-opcoes = input('[1] animes\n[2] Jogos\n> ')
+opcoes = input('[1] animes\n[2] Jogos\n[3] Aplicativos\n> ')
 thing = int(opcoes)
 
 if opcoes == '1':
@@ -85,6 +85,26 @@ elif opcoes == '2':
             i = input()
 
         action(site, game, thing,number)
+
+elif opcoes == '3':
+    app = str(input('\nNome do aplicativo:\n> ')).replace(' ', '+').lower()
+    
+    if app == '':
+        print('\nNão tem como pesquisar "nada".\nPor favor, feche e abra o programa novamente!')
+        i = input()
+
+    else:
+        site = input('\n[1] APK Pure\n> ')
+        number = int(site)
+
+        if site == '1':
+            site = 'apkpure.com/br'
+
+        else:
+            print('\nOpção inválida.\nPor favor, feche e abra o programa novamente!')
+            i = input()
+
+        action(site, app, thing,number)
 
 else:
     print('\nOpção inválida.\nPor favor, feche e abra o programa novamente!')
